3564f0c6150b51472df0427cead288957ceb2521
Silouanos <csilouanos@gmail.com>
2020-11-03 09:47:20 +0200

    Added more permission classes to views

--- apps/certificates/views.py
+++ apps/certificates/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .models import Certificate
 from .serializers import CertificateSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class CertificateViewSet(viewsets.ModelViewSet):
     queryset = Certificate.objects.all()
     serializer_class = CertificateSerializer
+    permission_classes = [IsAuthenticated]
--- apps/lectures/views.py
+++ apps/lectures/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .models import Lecture
 from .serializers import LectureSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class LectureViewSet(viewsets.ModelViewSet):
     queryset = Lecture.objects.all()
     serializer_class = LectureSerializer
+    permission_classes = [IsAuthenticated]
